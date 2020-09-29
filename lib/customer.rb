a2eee631491ddeb4e4d328232d7a11891e2d5648
Rafael Garcia <rafagrcb@gmail.com>
2020-09-29 16:59:27 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,12 +1,11 @@
 class Customer
-  attr_accessor :name, :age, :meals
+  attr_accessor :name, :age
   
   @@all = []
   
   def initialize(name, age)
     @name = name
     @age = age
-    @meals = meals
     @@all << self
   end
   
